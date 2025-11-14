9d29395619eb4aa6b3ffaf289ede08f61a56b42b
AlexxKai <alexurumart@gmail.com>
2025-11-14 01:18:53 +0100

    Lista desplegable funcional

--- app/src/main/java/com/example/listadesplegable/MainActivity.kt
+++ app/src/main/java/com/example/listadesplegable/MainActivity.kt
@@ -13,25 +13,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -82,8 +76,7 @@ class MainActivity : ComponentActivity() {
                         )
                         {
                             //Cuandro para entregar
-                            LazyColumn(
-                            ) {
+                            LazyColumn {
                                 //Un item simple
                                 item {
                                     Row(
@@ -110,36 +103,33 @@ class MainActivity : ComponentActivity() {
                                                     .size(100.dp),
                                                 color = Color.White
                                             )
-                                            Text(
-                                                text = "Descripción",
-                                                modifier = Modifier
-                                                    .size(70.dp),
-                                                color = Color.Magenta
-                                            )
+                                        }
+                                        var expanded by remember { mutableStateOf(false) }
+                                        Column(Modifier.clickable { expanded = !expanded }) {
+                                            AnimatedVisibility(!expanded) {
+                                                Text(
+                                                    text = "Descripción",
+                                                    modifier = Modifier
+                                                        .size(70.dp),
+                                                    color = Color.Magenta
+                                                )
+                                            }
+                                            AnimatedVisibility(expanded) {
+                                                Text(
+                                                    text = "Descripción completa",
+                                                    modifier = Modifier
+                                                        .size(70.dp),
+                                                    color = Color.Magenta
+                                                )
+                                            }
                                         }
                                     }
-
                                 }
+
                             }
                         }
                     }
                 }
-            }
-        }
-    }
-}
-
-@Composable
-fun Carta() {
-    Card {
-        var expanded by remember { mutableStateOf(false) }
-        Column(Modifier.clickable { expanded = !expanded }) {
-            Image(painterResource(R.drawable.lp))
-            AnimatedVisibility(expanded) {
-                Text(
-                    text = "Linkin Park",
-                    style = MaterialTheme.typography.bodyLarge
-                )
             }
         }
     }
